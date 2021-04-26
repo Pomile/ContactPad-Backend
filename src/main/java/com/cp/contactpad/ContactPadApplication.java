be79e3392e043c1b9e8d5d828c219358bba98520
babatundeogeds <babatunde.ogedengbe@thebulb.africa>
2021-04-26 05:07:16 +0100

    Initial commit

---
+++ src/main/java/com/cp/contactpad/ContactPadApplication.java
@@ -0,0 +1,13 @@
+package com.cp.contactpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactPadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactPadApplication.class, args);
+    }
+
+}
